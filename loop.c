b1a7adcb64fa798ccde2cc7d525be57d4bb70025
Deepak <rohit45deepak@gmail.com>
2025-01-31 23:42:21 +0530

    loop

---
+++ loop.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{ 
+    int num = 1;
+    for(num ; num<10; num++)
+    {
+
+        if(num%2==0)
+        { 
+           printf("%d number even\n", num);
+        }
+        
+       
+    }
+
+
+}
