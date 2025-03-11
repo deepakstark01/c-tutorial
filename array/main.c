64a40ab4a0239f5b72ce6746bc1a56724f8cc362
Deepak <60574055+deepakstark01@users.noreply.github.com>
2025-03-11 16:24:36 +0000

    array

--- array/main.c
+++ array/main.c
@@ -1,1 +1,39 @@
+#include<stdio.h>
+void main()
+{
+    int A[2][3]={
+        {2,4,1},
+        {5,7,9}
+    };
 
+    int B[3][2];
+    printf("Befor Transpose\n");
+    for(int i=0 ; i<2 ;i++ )
+    {
+        for(int j = 0 ; j<3; j++)
+        {
+            printf("%d ",A[i][j]);
+        }
+        printf("\n");
+    }
+    printf("After Transpose\n");
+
+    for(int i=0 ; i<3 ;i++ )
+    {
+        for(int j = 0 ; j<3; j++)
+        {
+            B[i][j]=A[j][i];
+        }
+      
+    }
+
+    for(int i=0 ; i<3 ;i++ )
+    {
+        for(int j = 0 ; j<2; j++)
+        {
+            printf("%d ",B[i][j]);
+        }
+        printf("\n");
+    }
+
+}
